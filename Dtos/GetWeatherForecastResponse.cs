f2b853bfe04dc6e1f728a657c7b32fa23090e967
Vishwam <vishwamkumar@outlook.com>
2024-07-02 11:03:25 -0400

    Initial commit

---
+++ Dtos/GetWeatherForecastResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace RedisWeatherService.Dtos;
+
+public class GetWeatherForecastResponse
+{
+    public string City { get; set; } = null!;
+    public DateTime ForecastTime { get; set; }
+    public IEnumerable<WeatherForecast> WeatherForecasts { get; set; } = [];
+    public string RabbitMqSendStatus { get; set; } = null!;
+    public string RabbitMqReceivedStatus { get; set; } = null!;
+}
---
+++ GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Vishwam.RabbitMqLibrary.Queuing.Services;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Logging;
+global using RedisWeatherService.Dtos;
+global using System.Text.Json;
